6bdc2dfaff4893a4c72da6fb0daa27e9412f0c3a
edzdez <ezsland05@gmail.com>
2021-06-23 17:23:55 -0700

    Cleaned up the code a bit

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,38 @@
 
 #include "../include/RenderWindow.h"
 
+bool init();
+
+void renderBoxes();
+
+void pollEvent();
+
+void sort();
+
+enum State {
+    STOPPED,
+    RUNNING,
+    SORTING,
+    PAUSED,
+    COMPLETED,
+};
+
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 480;
+
 const int NUM_BARS = 100;
 
 RenderWindow window;
+State state = STOPPED;
+
 std::vector<SDL_Rect> bars;
 
-bool init()
-{
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std:printf("Failed to intialize SDL2. Error: %s\n", SDL_GetError());
+int currPos = 0;
+
+bool init() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std:
+        printf("Failed to intialize SDL2. Error: %s\n", SDL_GetError());
         return false;
     }
 
@@ -27,8 +47,7 @@ bool init()
 
     srand(time(nullptr));
 
-    for (int i = 0; i < NUM_BARS; ++i)
-    {
+    for (int i = 0; i < NUM_BARS; ++i) {
         int height = rand() % ((SCREEN_HEIGHT / 4) * 3) + 50;
 
         SDL_Rect bar;
@@ -37,87 +56,86 @@ bool init()
         bar.w = SCREEN_WIDTH / NUM_BARS - 1;
         bar.h = height;
 
-//        bar.x = i * SCREEN_WIDTH / NUM_BARS;
-//        bar.y = SCREEN_HEIGHT - 100;
-//        bar.w = SCREEN_WIDTH / NUM_BARS;
-//        bar.h = 100;
-
         bars.push_back(bar);
     }
 
     return true;
 }
 
+void renderBoxes() {
+    window.clear();
+
+    for (int i = 0; i < NUM_BARS; ++i) {
+        bars[i].x = i * SCREEN_WIDTH / NUM_BARS;
+        window.render(&bars[i]);
+    }
+
+    window.display();
+}
+
+void pollEvent() {
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+        switch (e.type) {
+            case SDL_QUIT:
+                state = STOPPED;
+                break;
+            case SDL_KEYDOWN:
+                switch (e.key.keysym.sym) {
+                    case SDLK_q:
+                        state = STOPPED;
+                        break;
+                    case SDLK_SPACE:
+                        if (state != SORTING)
+                            state = SORTING;
+                        else
+                            state = PAUSED;
+
+                        break;
+                }
+                break;
+        }
+    }
+}
+
 void sort() {
-    for (int i = 0; i < NUM_BARS; ++i)
-    {
+    for (; currPos < NUM_BARS; ++currPos) {
+        if (state == PAUSED)
+            return;
+
         int swaps = 0;
-        for (int j = NUM_BARS - 1; j >= 0; --j)
-        {
-            if (j == i) break;
-            if (bars[j].h < bars[j - 1].h)
-            {
+        for (int j = NUM_BARS - 1; j > currPos; --j) {
+            if (bars[j].h < bars[j - 1].h) {
                 ++swaps;
                 SDL_Rect temp = bars[j];
                 bars[j] = bars[j - 1];
                 bars[j - 1] = temp;
             }
 
-            window.clear();
-            for (int k = 0; k < NUM_BARS; ++k)
-            {
-                bars[k].x = k * SCREEN_WIDTH / NUM_BARS;
-                window.render(&bars[k]);
-            }
+            pollEvent();
 
-            window.display();
+            renderBoxes();
             SDL_Delay(30);
         }
 
-        if (!swaps) break;
+        if (!swaps)
+            break;
     }
 
-    std::printf("completed\n");
+    state = COMPLETED;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
     init();
 
-    bool running = true;
-    while (running)
-    {
-        SDL_Event e;
-        while (SDL_PollEvent(&e))
-        {
-            switch(e.type)
-            {
-                case SDL_QUIT:
-                    running = false;
-                    break;
-                case SDL_KEYDOWN:
-                    switch(e.key.keysym.sym)
-                    {
-                        case SDLK_SPACE:
-                            std::printf("space pushed\n");
-                            sort();
+    state = RUNNING;
+    while (state != STOPPED) {
+        pollEvent();
 
-                            break;
-                    }
-                    break;
-            }
-        }
+        if (state == SORTING)
+            sort();
 
-
-        window.clear();
-        for (int i = 0; i < NUM_BARS; ++i)
-        {
-            bars[i].x = i * SCREEN_WIDTH / NUM_BARS;
-            window.render(&bars[i]);
-        }
-//        std::printf("\n");
-
-        window.display();
+        renderBoxes();
         SDL_Delay(300);
     }
 
